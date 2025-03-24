b8ca257f02762a5a7e2bfb8f9ad0a56572d2326a
Alisha296 <96118735+Alisha296@users.noreply.github.com>
2025-03-24 23:02:20 +0000

    Update settings.py

--- the_fashion_atelier/settings.py
+++ the_fashion_atelier/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clothes',
+    'the_fashion_atelier.clothes',
 ]
 
 MIDDLEWARE = [
